9eeb7c1dcd6a34fcb4918614b3968ed4f1066518
DSjet <59322818+DSjet@users.noreply.github.com>
2022-08-15 23:33:51 +0700

    Added Turn Base Combat Mechanics
    Added Turn Base Combat Mechanics
    Change build target to windows
    deprecate previous player movement

---
+++ Assets/Scripts/BattleHandler.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleHandler : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    StatsManager playerStats;
+    StatsManager enemyStats;
+
+    public Transform playerBattleStation;
+    public Transform enemyBattleStation;
+
+    public BattleState state;
+
+    void Start()
+    {
+        state = BattleState.START;
+        StartCoroutine(SetupBattle());
+    }
+
+    IEnumerator SetupBattle()
+    {
+
+        GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
+        playerStats = playerGO.GetComponent<StatsManager>();
+        GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
+        enemyStats = enemyGO.GetComponent<StatsManager>();
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
+    }
+
+    IEnumerator PlayerAttack()
+    {
+        bool isDead = enemyStats.TakeDamage(playerStats.damage);
+        
+        yield return new WaitForSeconds(2f);
+
+        if (isDead)
+        {
+            state = BattleState.WON;
+            EndBattle();
+        } else 
+        {
+            state = BattleState.ENEMYTURN;
+            StartCoroutine(EnemyTurn());
+        }
+    }
+
+    IEnumerator EnemyTurn()
+    {
+        // add some kind of animation or dialogues
+
+        yield return new WaitForSeconds(1f);
+
+        bool isDead = playerStats.TakeDamage(enemyStats.damage);
+
+        yield return new WaitForSeconds(1f);
+
+        if (isDead){
+            state = BattleState.LOST;
+            EndBattle();
+        } else {
+            state = BattleState.PLAYERTURN;
+            PlayerTurn();
+        }
+    }
+
+    void EndBattle()
+    {
+        if(state == BattleState.WON)
+        {
+            // display winning text
+        } else if (state == BattleState.LOST) {
+            // display losing text
+        }
+    }
+
+    private void PlayerTurn()
+    {
+        
+    }
+
+    public void OnAttackButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
+        
+        StartCoroutine(PlayerAttack());
+    }
+
+    public void OnHealButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
+        
+        StartCoroutine(PlayerHeal());
+    }
+
+    IEnumerator PlayerHeal()
+    {
+        playerStats.Heal(5);
+        
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.ENEMYTURN;
+        StartCoroutine(EnemyTurn());
+    }
+}
---
+++ Assets/Scripts/StatsManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsManager : MonoBehaviour
+{
+    public string unitName;
+    public int unitLevel;
+
+    public int damage;
+
+    public int maxHP;
+    public int currentHP;
+
+    public bool TakeDamage(int dmg){
+        currentHP -= dmg;
+
+        if (currentHP <= 0)
+            return true;
+        else
+            return false;
+    }
+
+    public void Heal(int amount){
+        currentHP += amount;
+        if (currentHP > maxHP)
+            currentHP = maxHP;
+    }
+}
