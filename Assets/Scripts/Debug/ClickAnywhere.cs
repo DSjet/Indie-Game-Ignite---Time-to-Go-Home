44e0fb91ffda32d010af0f5bcd91e00c1968352f
feliseureka <feliseureka@gmail.com>
2022-08-18 22:30:47 +0700

    Added prolog scenes

--- Assets/Scripts/Debug/ClickAnywhere.cs
+++ Assets/Scripts/Debug/ClickAnywhere.cs
@@ -1,16 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ClickAnywhere : MonoBehaviour
 {
+    public UnityEvent ev;
     // Update is called once per frame
     void Update()
     {
         if(Input.GetMouseButtonDown(0)){
-            if(FindObjectOfType<BubbleSystems>().isDialogueOpen){
-                FindObjectOfType<BubbleSystems>().showNextDialogue();
-            }
+            ev?.Invoke();
         }
     }
 }
